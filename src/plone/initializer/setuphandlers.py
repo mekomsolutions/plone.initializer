6126e515b607a229221106a1d8f28d2678ec55ee
Emmanuel Nyachoke <achachiez@gmail.com>
2021-07-05 09:42:25 +0300

    Add initialization functionality

--- src/plone/initializer/setuphandlers.py
+++ src/plone/initializer/setuphandlers.py
@@ -13,11 +13,28 @@ class HiddenProfiles(object):
         ]
 
 
+def _load_senaite_data(context):
+    portal_setup = getToolByName(context, 'portal_setup')
+    tar_file_path =  os.path.join(os.path.dirname(getConfiguration().clienthome),'','importdata','data.tar.gz')
+    if Path(tar_file_path).exists():
+        tar_fileobj = io.BytesIO()
+        with open(tar_file_path, 'r:*') as fin:
+            tar_fileobj = io.BytesIO(fin.read())
+        tarball = tar_fileobj.getvalue()
+        result = portal_setup.runAllImportStepsFromProfile(None, True, archive=tarball)
+        steps_run = 'Steps run: %s' % ', '.join(result['steps'])
+        portal_setup.manage_importSteps(manage_tabs_message=steps_run,
+                                        messages=result['messages'])
+
+def _remove_plugin(pas, pluginid):
+    if pluginid in pas.objectIds():
+        pas.manage_delObjects([pluginid])
+
+
 def post_install(context):
     """Post install script"""
-    # Do something at the end of the installation of this package.
-
+    aclu = getSite().acl_users
+    _load_senaite_data(context)
 
 def uninstall(context):
     """Uninstall script"""
-    # Do something at the end of the uninstallation of this package.
